50e86335b054323fdd2567ab0f8a57642bf9d2bf
rdmadeira <rdmadeira2@gmail.com>
2023-03-15 20:22:39 -0300

    res getStaticProps and ...Paths getting data

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,4 +1,6 @@
 import { Box, Text, Button } from '@chakra-ui/react';
+import { connectDB } from '../../db/connection';
+import { getPosts } from '../../db/post_utils';
 
 const Post = ({ post }) => {
   return (
@@ -31,13 +33,18 @@ const Post = ({ post }) => {
 // Funcion de nextjs para acceder a los paths, que en el bilding de la app, no existen los paths. Se puede acceder por fetch a mongodb, o sacar de la base de datos db
 
 export async function getStaticPaths() {
-  const result = await fetch(
+  /* const result = await fetch(
     `${process.env.PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/post`
-  ); // por defecto el fetch es GET
-  const { data } = await result.json();
+    ); // por defecto el fetch es GET
+    const { data } = await result.json(); */
+  // Esto arriba no tiene sentido porque está tratando de acceder a la ruta de la misma app la cual todavía no se construyó.
+  // Se accede directamente a la base de datos en este caso.
+
+  const { db } = await connectDB();
+  const posts = await getPosts(db);
 
   const paths =
-    data?.map((post) => ({
+    posts?.map((post) => ({
       params: {
         id: post._id,
       },
@@ -48,14 +55,19 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // params es la prop del contexto (context.params)
-  const res = await fetch(
+  /* const res = await fetch(
     `${process.env.PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/${params.id}`
   );
-  const { data } = await res.json();
+  const { data } = await res.json(); */
+  // Esto arriba no tiene sentido porque está tratando de acceder a la ruta de la misma app la cual todavía no se construyó.
+  // Se accede directamente a la base de datos en este caso.
+
+  const { db } = await connectDB();
+  const post = await getPosts(db, params.id);
 
   return {
     props: {
-      post: data[0],
+      post: post[0],
     },
   };
 }
